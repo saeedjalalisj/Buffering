b53e49db6c41c214cbcdaab5d85bb79d03c625ab
saeed jalali <saeedjalalisj@gmail.com>
2019-12-08 22:40:36 +0330

    add simple local-passport

---
+++ src/users/users.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = any;
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[];
+
+  constructor() {
+    this.users = [
+      {
+        userId: 1,
+        username: 'test1',
+        password: 'pass1',
+      },
+      {
+        userId: 2,
+        username: 'test2',
+        password: 'pass2',
+      },
+    ];
+  }
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username);
+  }
+}
