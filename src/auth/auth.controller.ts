83552a388d4fcafb875649392cf0961296362cdf
saeedjalali <saeedjalalisj@gmail.com>
2019-12-14 16:48:58 +0330

    fix tslint problem

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Request, Get ,Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Get , Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 
