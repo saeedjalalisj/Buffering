fafc04eeaabd79de07c62960d5b1474c5a1f07e1
saeedjalali <saeedjalalisj@gmail.com>
2019-12-23 16:41:41 +0330

    add company module and service generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { DatabaseModule } from './config/database.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { RoleModule } from './role/role.module';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, UsersModule, RoleModule],
+  imports: [DatabaseModule, AuthModule, UsersModule, RoleModule, CompanyModule],
   controllers: [AppController],
   providers: [AppService],
 })
