efc9535a5472a1503dddd22eb2f3d72c67338721
saeedjalali <saeedjalalisj@gmail.com>
2019-12-23 16:02:17 +0330

    add test for role

--- src/role/role.service.spec.ts
+++ src/role/role.service.spec.ts
@@ -1,29 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { RoleService } from './role.service';
-import {TypeOrmModule} from '@nestjs/typeorm';
 import {Role} from './role.entity';
 
 describe('RoleService', () => {
   let service: RoleService;
+  let roleRepository: Repository<Role>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoleService],
-      imports: [TypeOrmModule.forFeature([Role])],
+      providers: [
+        RoleService,
+        {
+          provide: getRepositoryToken(Role),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<RoleService>(RoleService);
+
+    roleRepository = module.get<Repository<Role>>(getRepositoryToken(Role));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it('should be defined findById', () => {
+  it('should be defined findByRole', () => {
     expect(service.findById).toBeDefined();
+  });
+
+  it('findById return role by id', async () => {
+    const result: Role = { title: '', id: 1, description: '' };
+    const mockRleId = '1';
+    jest.spyOn(roleRepository, 'findOne').mockResolvedValueOnce(result);
+    expect( await service.findById(mockRleId)).toEqual(result);
   });
 
   it('should be defined findByRole', () => {
     expect(service.findByRole).toBeDefined();
+  });
+
+  it('findByRole return role by role name', async () => {
+    const result: Role = { title: 'admin', id: 1, description: '' };
+    const mockRleId = 'admin';
+    jest.spyOn(roleRepository, 'findOne').mockResolvedValueOnce(result);
+    expect( await service.findByRole(mockRleId)).toEqual(result);
   });
 });
